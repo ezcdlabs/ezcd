91149aea306eccde6fd068cd9aec0ccdc5016fb5
Steven Borrie <saborrie@gmail.com>
2024-11-12 18:27:38 +0000

    added additonal info into playwright report to help diagnose acceptance failures

--- acceptance/tests/dsl/CliDriver.ts
+++ acceptance/tests/dsl/CliDriver.ts
@@ -1,21 +1,29 @@
 import { promisify } from 'util';
 import { exec as execCallback } from 'child_process';
-
+import { test } from '@playwright/test'
 const exec = promisify(execCallback);
 
 async function ezcdCli(args: string) {
-  try {
-    const { stdout, stderr } = await exec(`../dist/ezcd-cli ${args}`, {
-      env: {
-        EZCD_DATABASE_URL: process.env.EZCD_DATABASE_URL,
-      }
-    });
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-    return stdout;
-  } catch (error) {
-    throw new Error(`Failed to run ezcd-cli. EZCD_DATABASE_URL was '${process.env.EZCD_DATABASE_URL}' error:${error} stdout:${error.stdout} stderr:${error.stderr}`);
-  }
+  return test.step(`ezcd-cli ${args}`, async () => {
+    try {
+      const { stdout, stderr } = await exec(`../dist/ezcd-cli ${args}`, {
+        env: {
+          EZCD_DATABASE_URL: process.env.EZCD_DATABASE_URL,
+        }
+      });
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
+
+      // record the result in playwright report:
+      test.info().attach(`ezcd-cli ${args}`, {
+        body: `stdout: ${stdout}\n\nstderr: ${stderr}`,
+      });
+
+      return stdout;
+    } catch (error) {
+      throw new Error(`Failed to run ezcd-cli. EZCD_DATABASE_URL was '${process.env.EZCD_DATABASE_URL}' error:${error} stdout:${error.stdout} stderr:${error.stderr}`);
+    }
+  });
 }
 
 export default class CLIDriver {
