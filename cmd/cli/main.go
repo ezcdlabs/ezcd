1fa401b1ba13278d57d7c061b9e9bf42f876ed96
Steven Borrie <saborrie@gmail.com>
2024-11-23 14:00:50 +0000

    goreleaser wanted everything in one file

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -4,9 +4,12 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/ezcdlabs/ezcd/cmd/cli/cmd"
+	"github.com/ezcdlabs/ezcd/pkg/ezcd"
+	"github.com/ezcdlabs/ezcd/pkg/ezcd_postgres"
 )
 
 var version = "dev" // default version
@@ -19,4 +22,22 @@ func main() {
 	if err != nil {
 		os.Exit(1)
 	}
+}
+
+func NewEnvServiceLoader() envServiceLoader {
+	return envServiceLoader{}
+}
+
+type envServiceLoader struct{}
+
+func (envServiceLoader) Load() (ezcd.Ezcd, error) {
+	ezcdDatabaseUrl := os.Getenv("EZCD_DATABASE_URL")
+
+	if ezcdDatabaseUrl == "" {
+		return nil, fmt.Errorf("database connection string is required, please set EZCD_DATABASE_URL")
+	}
+
+	ezcdService := ezcd.NewEzcdService(ezcd_postgres.NewPostgresDatabase(ezcdDatabaseUrl))
+
+	return ezcdService, nil
 }
